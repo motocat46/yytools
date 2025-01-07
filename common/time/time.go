80665570f6f0521b1928a3fd449d892f5ab26bfd
yangyuan <yytqmj@vip.qq.com>
2025-01-07 16:35:19 +0800

    解析时间长度字符串的函数 增加数值溢出检查

--- common/time/time.go
+++ common/time/time.go
@@ -18,65 +18,104 @@
 package time
 
 import (
-    "errors"
-    "fmt"
-    "strconv"
-    "strings"
-    "time"
+	"errors"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"time"
 )
+
+// 检查是否会溢出，溢出则返回true
+func mul(a, b int64) (int64, bool) {
+	if a == 0 || b == 0 {
+		return 0, false
+	}
+	result := a * b
+	return result, result/b != a || result/a != b
+}
+
+// 检查是否会溢出，溢出则返回true
+func add(a, b int64) (int64, bool) {
+	if (b > 0 && a > math.MaxInt64-a) || (b < 0 && a < math.MinInt64-b) {
+		return a + b, true
+	}
+	return a + b, false
+}
+
+// 检查是否会溢出，溢出则返回true
+func sub(a, b int64) (int64, bool) {
+	if (b < 0 && a > math.MaxInt64+b) || (b > 0 && a < math.MinInt64+b) {
+		return a - b, true
+	}
+	return a - b, false
+}
+
 
 // 在标准库基础上，支持更大的日期单位——d（日）
 // 最大时长近似290年
 func ParseDuration(s string) (time.Duration, error) {
-    // 支持特殊格式：空或者0，都返回0
-    if s == "" || s == "0" {
-        return 0, nil
-    }
-    
-    // 这里额外做个长度判断(理论上输入的时间长度字符串不会超过这个长度)
-    // 避免处理错误的输入数据
-    if len(s) > 100 {
-        return 0, errors.New(fmt.Sprintf("time string s is too big:%s", s))
-    }
-    
-    // 因为在库函数的基础上增加了天数的支持,就要在判断一下字符串中间是否还有正负号
-    // 剩下的检查交给库函数
-    for i := 1; i < len(s); i++ {
-        // -和+只允许出现在第一个字符
-        if s[i] == '-' || s[i] == '+' {
-            return 0, errors.New(fmt.Sprintf("invalid duration time s:%s", s))
-        }
-    }
-    
-    // 处理一下，将可能的大写字母转换成小写字母(增加容错率)
-    s = strings.ToLower(s)
-    // 判断一下是否有天的时间单位
-    index := strings.IndexRune(s, 'd')
-    if index == -1 {
-        // 没有单位'd'，则直接调用系统原生函数
-        return time.ParseDuration(s)
-    }
-    
-    left, right := s[:index], s[index+1:]
-    days, err := strconv.Atoi(left)
-    if err != nil {
-        return 0, err
-    }
-    
-    // TODO yy 处理数值溢出的情况
-    daysPart := time.Hour * 24 * time.Duration(days)
-    if len(right) == 0 {
-        return daysPart, nil
-    }
-    
-    duration, err := time.ParseDuration(right)
-    if err != nil {
-        return 0, err
-    }
-    
-    // 要考虑负数的情况(这种情况下，传到系统函数时，就没有负数标志了)
-    if days < 0 {
-        return daysPart - duration, nil
-    }
-    return daysPart + duration, nil
+	// 支持特殊格式：空或者0，都返回0
+	if s == "" || s == "0" {
+		return 0, nil
+	}
+	
+	// 这里额外做个长度判断(理论上输入的时间长度字符串不会超过这个长度)
+	// 避免处理错误的输入数据
+	if len(s) > 100 {
+		return 0, errors.New(fmt.Sprintf("time string s is too big:%s", s))
+	}
+	
+	// 因为在库函数的基础上增加了天数的支持,就要在判断一下字符串中间是否还有正负号
+	// 剩下的检查交给库函数
+	for i := 1; i < len(s); i++ {
+		// -和+只允许出现在第一个字符
+		if s[i] == '-' || s[i] == '+' {
+			return 0, errors.New(fmt.Sprintf("invalid remain time s:%s", s))
+		}
+	}
+	
+	// 处理一下，将可能的大写字母转换成小写字母(增加容错率)
+	s = strings.ToLower(s)
+	// 判断一下是否有天的时间单位
+	index := strings.IndexRune(s, 'd')
+	if index == -1 {
+		// 没有单位'd'，则直接调用系统原生函数
+		return time.ParseDuration(s)
+	}
+	
+	left, right := s[:index], s[index+1:]
+	days, err := strconv.Atoi(left)
+	if err != nil {
+		return 0, err
+	}
+	
+	daysPart, overflow := mul(int64(time.Hour)*24, int64(days))
+	if overflow {
+		return 0, errors.New(fmt.Sprintf("invalid duration overflow:%s", s))
+	}
+	
+	if len(right) == 0 {
+		return time.Duration(daysPart), nil
+	}
+	
+	remain, err := time.ParseDuration(right)
+	if err != nil {
+		return 0, err
+	}
+	
+	// 要考虑负数的情况(这种情况下，传到系统函数时，就没有负数标志了)
+	if days < 0 {
+		result, overflow := sub(daysPart, int64(remain))
+		if overflow {
+			return 0, errors.New(fmt.Sprintf("invalid duration overflow:%s", s))
+		}
+		return time.Duration(result), nil
+	}
+	
+	result, overflow := add(daysPart, int64(remain))
+	if overflow {
+		return 0, errors.New(fmt.Sprintf("invalid duration overflow:%s", s))
+	}
+	return time.Duration(result), nil
 }
