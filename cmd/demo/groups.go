a3796936c556dc91d28203e1c2311034cbc5574e
yangyuan <yytqmj@vip.qq.com>
2026-04-17 11:32:12 +0800

    feat(demo): groups.go 注册 5 个分组的 icon 和 desc

---
+++ cmd/demo/groups.go
@@ -0,0 +1,24 @@
+// 版权所有(Copyright)[yangyuan]
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// 作者:  yangyuan
+// 创建日期:2026/4/17
+package main
+
+func init() {
+	RegisterGroup("pkg/algorithms", "⚡", "排序、搜索、数学工具、ID 生成")
+	RegisterGroup("pkg/ds", "🗂️", "堆、队列、跳表、LRU、Trie")
+	RegisterGroup("pkg/infra", "🔧", "并发原语、定时器、安全执行")
+	RegisterGroup("pkg/mechanics", "🎮", "概率分布与周期引擎")
+	RegisterGroup("pkg/slicex", "📦", "切片工具函数")
+}
